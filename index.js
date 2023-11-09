2252fcf96c4f70f1e07b518cdabedf3c9e0d76a2
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-11-09 12:01:18 +0600

    added pagenation all foood page

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const cors = require('cors');
 // express app chalou korar jonno
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
+
+
+app.use(cors({
+
+    origin: ['http://localhost:5173', 'https://resturent-5e702.web.app'],
+    credentials: true // You can include this if you need to send cookies or authorization headers.
+}));
 
 // middleware use
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+
+
+app.use(cookieParser());
+
 
 
 // undurstanding root
@@ -29,25 +42,88 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+const logger = (req, res, next) => {
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    console.log('token in the middleware', token);
+    // no token available 
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+
+
+
 
         //------------------------- Restaurant Management all collection---------------------------------
 
         const foodCollection = client.db("restaurantDB").collection("foods")
         const orderCollection = client.db("restaurantDB").collection("carts")
+        const userAddCollection = client.db("restaurantDB").collection("users")
 
         //------Read------------my all app.get collection restaurantDB code start hare-----------Read---------
+        // auth related api
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
-        // get foods collection
-        app.get('/foods', async (req, res) => {
-            const cursor = foodCollection.find();
-            const result = await cursor.toArray()
-            res.send(result)
-
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+                // sameSite: 'none'
+            })
+                .send({ success: true });
         })
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logging out', user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+        // Get all foods or implement pagination
+        app.get('/foods', async (req, res) => {
+            const page = parseInt(req.query.page) || 1;
+            const pageSize = 9;
+
+            try {
+                if (page === 1) {
+                    // If page is 1, fetch all foods
+                    const cursor = foodCollection.find();
+                    const result = await cursor.toArray();
+                    res.send(result);
+                } else {
+                    // For other pages, implement pagination
+                    const cursor = foodCollection.find()
+                        .skip((page - 1) * pageSize)
+                        .limit(pageSize);
+
+                    const result = await cursor.toArray();
+                    res.send(result);
+                }
+            } catch (error) {
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+
 
         // this line code clint side home page top food collection app.get 
         app.get('/topFood', async (req, res) => {
@@ -74,35 +150,24 @@ async function run() {
             // console.log("find single data: ", result)
             res.send(result)
         })
-// ...
+        // ...
 
-// Read app.get orderCollection
-app.get('/orderingPage', async (req, res) => {
-    const cursor = orderCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-  });
-  
-  // Modify the /orderingPage/:id endpoint to subtract Quantity from foods collection
-  
-  
-  
+        // Read app.get orderCollection
+        app.get('/carts', logger, verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            console.log('token owner info', req.user)
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
 
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        });
 
-        //--------------------- below this code i do not use but it work perfect----------------------
-        // app.get('/Search', async (req, res) => {
-        //     // console.log("ami ata dekte chai",req.query.topFood);
-        //     let query = {};
-
-        //     if (req.query?.FoodName) {
-        //         // query = { email: req.query.email }
-        //         query.FoodName = {$regex: req.query.FoodName, $options:"i"}
-        //     }
-
-        //     const result = await foodCollection.find(query).toArray();
-        //     res.send(result);
-        // })
-        // -----------------------------------------------------------------------------------------------------
 
 
         app.post('/foods', async (req, res) => {
@@ -120,7 +185,33 @@ app.get('/orderingPage', async (req, res) => {
             const result = await orderCollection.insertOne(newOrder);
             res.send(result)
         })
+        app.post('/users', async (req, res) => {
+            const newOrder = req.body;
+            // console.log(newBrand)
+            const result = await userAddCollection.insertOne(newOrder);
+            res.send(result)
+        })
+        app.get('/users', async (req, res) => {
+            const cursor = userAddCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
 
+        })
+        // Read app.get orderCollection
+        app.get('/addedUser', async (req, res) => {
+            console.log(req.query.email);
+            console.log('token owner info', req.user)
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            const result = await userAddCollection.find(query).toArray();
+            res.send(result);
+        });
 
 
 
