65a3d3a5b7132f2c7e08d50dbd3c678155f887db
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-11-09 18:31:08 +0600

    added cors policy inside index.js

--- index.js
+++ index.js
@@ -12,12 +12,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
 
-    origin: ['http://localhost:5173', 'https://resturent-5e702.web.app'],
+    origin: ['https://resturent-5e702.web.app', 'https://resturent-5e702.firebaseapp.com' ,'http://localhost:5173'  ],
     credentials: true // You can include this if you need to send cookies or authorization headers.
 }));
 
+
+
+
 // middleware use
-// app.use(cors());
+
 app.use(express.json());
 
 
@@ -44,7 +47,7 @@ const client = new MongoClient(uri, {
 
 // middlewares
 const logger = (req, res, next) => {
-    console.log('log: info', req.method, req.url);
+    console.log('log info logger: ', req.method, req.url);
     next();
 }
 
@@ -153,12 +156,12 @@ async function run() {
         // ...
 
         // Read app.get orderCollection
-        app.get('/carts', logger, verifyToken, async (req, res) => {
-            console.log(req.query.email);
-            console.log('token owner info', req.user)
-            if (req.user.email !== req.query.email) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
+        app.get('/carts',  async (req, res) => {
+            // console.log("chek", req.query.email);
+            // console.log('token owner info', req.user)
+            // if (req.user.email !== req.query.email) {
+            //     return res.status(403).send({ message: 'forbidden access' })
+            // }
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -200,10 +203,10 @@ async function run() {
         // Read app.get orderCollection
         app.get('/addedUser', async (req, res) => {
             console.log(req.query.email);
-            console.log('token owner info', req.user)
-            if (req.user.email !== req.query.email) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
+            // console.log('token owner info', req.user)
+            // if (req.user.email !== req.query.email) {
+            //     return res.status(403).send({ message: 'forbidden access' })
+            // }
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -213,6 +216,18 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/userUpdate/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log("singleDetails id:  ",id)
+
+            const query = { _id: new ObjectId(id) }
+            // console.log("this is query:   ",query)
+
+            const result = await userAddCollection.findOne(query);
+            // console.log("find single data: ", result)
+            res.send(result)
+        })
+        // ...
 
 
 
@@ -226,10 +241,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
-
 
 
 
